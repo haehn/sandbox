dc3e40188157f7b90e3f988378c883c88fc15d57
haehn <haehn@seas.harvard.edu>
2015-02-10 14:29:00 -0500

    Stitching works (kind of).

--- fw2/_zstack/stitcher.py
+++ fw2/_zstack/stitcher.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 import pyopencl as cl
 
@@ -13,6 +14,45 @@ class Stitcher(Worker):
     # setup stitch kernel
     stitcher = Powertrain(True)
     stitcher.program = """
+
+
+        __kernel void stitch2(__global uchar *out_g,
+                                const int out_width,
+                                const int out_height,  
+                                __global const uchar *tile_g) {
+          int gid = get_global_id(0);
+
+          if (gid >= out_width*out_height)
+            return;
+
+          // col + row inside output
+          int col = gid % out_width;
+          int row = gid / out_width;
+
+          if (tile_g[gid] == 0) {
+
+            // check for boundary pixels (thin black lines)
+            //int k_top = (row+1)*out_width + col;
+            //int k_left = row*out_width + col+1;
+
+            //if (tile_g[k_top] != 0) {
+            //  out_g[gid] = tile_g[k_top];
+            //  return;
+            //}
+
+            //if (tile_g[k_left] != 0) {
+            //  out_g[gid] = tile_g[k_left];
+            //  return;
+            //}
+
+            return;
+          }
+
+          out_g[gid] = tile_g[gid];
+
+
+        }
+
         __kernel void stitch(__global uchar *out_g,
                                 const int out_width,
                                 const int out_height,
@@ -93,24 +133,42 @@ class Stitcher(Worker):
       print 'placing tile', t, 'at', offset_x, offset_y
 
       mf = cl.mem_flags
-      out_img = cl.Buffer(stitcher.context, mf.READ_WRITE | mf.USE_HOST_PTR, hostbuf=view._memory)
+
+      output_subarray_start = offset_y*out_width + offset_x
+      output_subarray_end = output_subarray_start + tile_width*tile_height
+      output_subarray = view._imagedata[output_subarray_start:output_subarray_end]
+
+      out_img = cl.Buffer(stitcher.context, mf.WRITE_ONLY | mf.USE_HOST_PTR, hostbuf=output_subarray)
       in_img = cl.Buffer(stitcher.context, mf.READ_ONLY | mf.USE_HOST_PTR, hostbuf=t._memory)
 
-      stitcher.program.stitch(stitcher.queue,
-                              (out_width*out_height,),
-                              None,
-                              out_img,
-                              np.int32(out_width),
-                              np.int32(out_height),
-                              np.int32(offset_x),
-                              np.int32(offset_y),
-                              np.int32(tile_width),
-                              np.int32(tile_height),
-                              in_img
+      # stitcher.program.stitch(stitcher.queue,
+      #                         (out_width*out_height,),
+      #                         None,
+      #                         out_img,
+      #                         np.int32(out_width),
+      #                         np.int32(out_height),
+      #                         np.int32(offset_x),
+      #                         np.int32(offset_y),
+      #                         np.int32(tile_width),
+      #                         np.int32(tile_height),
+      #                         in_img
 
-                              )
+      #                         )
+      stitcher.program.stitch2(stitcher.queue,
+                               (tile_width*tile_height,),
+                               None,
+                               out_img,
+                               np.int32(tile_width),
+                               np.int32(tile_height),
+                               in_img)
 
-    cl.enqueue_copy(stitcher.queue, view._memory, out_img).wait()
+      cl.enqueue_copy(stitcher.queue, output_subarray, out_img).wait()
+
+      view._imagedata[output_subarray_start:output_subarray_end] = output_subarray
+
+
+    img = view._imagedata.reshape(out_height, out_width)
+    cv2.imwrite('/tmp/stitch.jpg', img)
 
     manager.onStitch(view)
 
